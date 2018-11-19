8ced135e2cd6825eddc192022c731eb9970740be
Aleksander Kryukov <saskryukov@yandex.ru>
2018-11-19 22:51:02 +0100

    OLSong

--- Controller.java
+++ Controller.java
@@ -4,16 +4,19 @@ import models.TicketsBase;
 
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Controller{	
 	Scanner sc = new Scanner(System.in);
 	GoldenTicket goldenTickets;
+	OLS OLSong;
 
 	public Controller(){
 		ArrayList<GoldenTicket> goldenTickets = new ArrayList<>();
+		OLSong = new OLS();
 	}
 
-	public void addTicket(){
+	/*public void addTicket(){
 		System.out.println("Write the ticket's code");
 		String code = sc.next();			
 		
@@ -23,5 +26,14 @@ public class Controller{
 		GoldenTicket gt = new GoldenTicket(code, raffled);
 		
 		goldenTickets.addTicket(gt);
-	}	
+	}*/
+
+	public void getSong(int num) throws Exception{
+		for (int i = 0; i < num; i++){
+			Random rand = new Random();
+			int size = OLSong.getLine().size() - 1;
+			int randLine = rand.nextInt(size)+1;
+			System.out.println(OLSong.getLine().get(randLine));
+		}
+	}
 }
--- models/OLS.java
+++ models/OLS.java
@@ -1,10 +1,28 @@
 package models;
 
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.io.FileReader;
+import java.util.List;
+
 public class OLS{
 	//Attributes
+	private int lines;
 
 	//Constructor
-	public OLS(){
-		
+	public OLS(){}
+
+	public int getLines(){
+		return lines;
+	}
+
+	public List<String> getLine() throws Exception{
+		BufferedReader reader = new BufferedReader(new FileReader("D:\\Second desktop\\git\\models\\OompaLoompaSong.txt"));
+		String line;
+        List<String> list = new ArrayList<String>();
+        while ((line = reader.readLine()) != null) {
+            list.add(line);
+        }
+        return list;
 	}
 }
